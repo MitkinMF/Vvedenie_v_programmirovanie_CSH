cc0608cde35f2bd949c008d23e8ead12ae464f1d
Mikhail Mitkin <mitkin3145@yandex.ru>
2022-07-16 15:55:54 +0500

    Add 023

--- 023/Program.cs
+++ 023/Program.cs
@@ -1,1 +1,23 @@
 ﻿//Найти расстояние между точками в пространстве 2D
+
+double x1,x2,y1,y2, dist;
+
+System.Console.WriteLine("введите кординату x1 точки A");
+string? s=Console.ReadLine(); //ввод данных 
+x1=Convert.ToDouble(s); // конвертация в double
+
+System.Console.WriteLine("введите кординату y1  точки A");
+s=Console.ReadLine();   //ввод данных 
+y1=Convert.ToDouble(s); // конвертация в double
+
+System.Console.WriteLine("введите кординату x2  точки B ");
+s=Console.ReadLine();   //ввод данных c
+x2=Convert.ToDouble(s); // конвертация в double
+
+System.Console.WriteLine("введите кординату y2  точки B");
+s=Console.ReadLine();   //ввод данных 
+y2=Convert.ToDouble(s); // конвертация в double
+
+dist=Math.Sqrt (Math.Pow((x2-x1), 2) + Math.Pow((y2-y1),2));  
+
+System.Console.WriteLine($"расстояние между точками A и B равно {dist}");
