801e97df86d72ab6e380e8898cd1e0b0345beea5
Mikhail Mitkin <mitkin3145@yandex.ru>
2022-07-16 15:09:46 +0500

    Add 021

--- 013/Program.cs
+++ 013/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine ("Введите целое число");
 string? s = Console.ReadLine();
 
-string? s_cut1 = s.Substring(0,s.Length-(s.Length-1)); // Обрезает первую цифру числа
-string? s_cut2 = s.Substring(2); //Обрезает число начиная с третьей цифры
+string s_cut1 = s.Substring(0,s.Length-(s.Length-1)); // Обрезает первую цифру числа
+string s_cut2 = s.Substring(2); //Обрезает число начиная с третьей цифры
 
 System.Console.WriteLine($"Число без второй цифры - {s_cut1 + s_cut2}");
---
+++ 021/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+
+double a;
+System.Console.WriteLine ("Введите номер четверти координатной плоскости");
+string? s=Console.ReadLine(); //ввод данных 
+a=Convert.ToDouble(s); // конвертация в double
+
+if (a==1) //проверка на 1-ую четверть
+{
+    System.Console.WriteLine("В 1-ой четверти диапазоны значений: х (0, +inf), у(0,++inf)");
+}
+
+else if (a==2) //проверка на 2-ую четверть
+{
+    System.Console.WriteLine("Во 2-ой четверти диапазоны значений: х(0, -inf), у(0,+inf)");
+}
+
+else if (a==3) //проверка на 3-ую четверть
+{
+    System.Console.WriteLine("В 3-ей четверти диапазоны значений: х(0, -inf), у(0,-inf)");
+}
+
+else if (a==4) //проверка на 4-ую четверть
+{
+    System.Console.WriteLine("В 4-ой четверти диапазоны значений: х(0, +inf), у(0,-inf)");
+}
+
+else
+{
+    System.Console.WriteLine("Не верный номер");
+}
