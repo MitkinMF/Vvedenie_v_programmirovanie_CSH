6ab7d067f513321c3b5e555d6b62595c49440633
Mikhail Mitkin <mitkin3145@yandex.ru>
2022-07-16 14:19:03 +0500

    Add 020

--- 019/Program.cs
+++ 019/Program.cs
@@ -7,4 +7,3 @@ y=false;
 f1=!(x || y);
 f2=!x && !y;
 System.Console.WriteLine($" {x}, {y}, {f1}, {f2}");
-
---
+++ 020/Program.cs
@@ -0,0 +1,37 @@
+﻿//Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+double x,y;
+System.Console.WriteLine ("введите координаты точки (x,y):");
+System.Console.WriteLine ("");
+System.Console.WriteLine("введите х:");
+string? s=Console.ReadLine(); //ввод данных x
+x=Convert.ToDouble(s); // конвертация в double
+
+System.Console.WriteLine("введите y:");
+s=Console.ReadLine();   //ввод данных y
+y=Convert.ToDouble(s); // конвертация в double
+
+if (x>0 && y>0) //проверка на 1-ую четверть
+{
+    System.Console.WriteLine($"Точка с координатами ({x},{y}) находится в 1-ой четверти системы координат");
+}
+
+else if (x<0 && y>0) //проверка на 2-ую четверть
+{
+    System.Console.WriteLine($"Точка с координатами ({x},{y}) находится в 2-ой четверти системы координат");
+}
+
+else if (x<0 && y<0) //проверка на 3-ую четверть
+{
+    System.Console.WriteLine($"Точка с координатами ({x},{y}) находится в 3-ей четверти системы координат");
+}
+
+else if (x>0 && y<0) //проверка на 4-ую четверть
+{
+    System.Console.WriteLine($"Точка с координатами ({x},{y}) находится в 4-ой четверти системы координат");
+}
+
+else if (x==0 && y==0)
+{
+    System.Console.WriteLine($"Точка является началом координат");
+}
