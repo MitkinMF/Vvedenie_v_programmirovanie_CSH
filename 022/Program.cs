80aabc8a7843cd1da0efc9f90a4c1be2e310564c
Mikhail Mitkin <mitkin3145@yandex.ru>
2022-07-16 15:40:03 +0500

    Add 022

---
+++ 022/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа проверяет пятизначное число на палиндром.
+
+Console.WriteLine ("Введите целое пятизначное число");
+string? s = Console.ReadLine();
+
+if (s.Length==5) //проверка на пятизначность
+{
+    if (s[0]==s[4] && s[1]==s[3] )  //проверка на палиндром
+    {
+        System.Console.WriteLine($"Число {s} - палиндром");
+    }
+
+    else
+    {
+        System.Console.WriteLine($"Число {s} - не палиндром");
+    }
+}    
+else
+{
+    System.Console.WriteLine($"Необходимо пятизначное число");
+}
