86bb639440686847468c08762005c8a4e8415704
«Mitkin <mitkin3145@yandex.ru>
2022-07-23 22:05:30 +0500

    Add 046

---
+++ 046/Program.cs
@@ -0,0 +1,32 @@
+﻿//46. Показать числа Фибоначчи
+
+
+int N=15;  //Количество элементов последовательности Фибоначи
+
+Fibonachi(N);
+
+//функция
+void Fibonachi(int N) //рассчитывает и отображает последовательность Фибоначи
+{
+switch(N)
+{
+       case 0:
+        System.Console.WriteLine($"F0=0 "); 
+        break;
+    case 1:
+        System.Console.Write($"F0=0 F1=1 "); 
+        break;
+    default:
+        double f0=0;
+        double f1=1;
+        System.Console.Write($"F0=0 F1=1 ");
+        for(int i=2; i<=N; i++)
+        {
+            double f=f1+f0;
+            System.Console.Write($"F{i}={f} ");
+            f0=f1;
+            f1=f;
+        }
+        break;
+}      
+}
